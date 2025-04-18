ec2f10b0b831e59616dc9efb5ab6908c67dcebc6
chandan <chandanp1291@gmail.com>
2025-04-18 05:27:46 +0000

    Your commit message here

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  count         = 2
+  count         = 1
   ami           = "ami-04f167a56786e4b09"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"               # Free-tier eligible instance type
   key_name      = "ansible-key"          # Replace with your actual EC2 key pair
 
   tags = {
-    Name = "TerraformDemo-${count.index + 1}" 
+    Name = "TerraformDemo
   }
 }
 
